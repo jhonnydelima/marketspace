48b7141edfd4c76744405e1b43763b902491d6cd
Jhonny Lima <lima.jhonny95@gmail.com>
2023-08-23 23:58:24 -0300

    chore: set up path mapping

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,25 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          alias: {
+            '@assets': './src/assets',
+            '@components': './src/components',
+            '@contexts': './src/contexts',
+            '@dtos': './src/dtos',
+            '@hooks': './src/hooks',
+            '@routes': './src/routes',
+            '@screens': './src/screens',
+            '@services': './src/services',
+            '@storage': './src/storage',
+            '@utils': './src/utils'
+          }
+        },
+      ],
+    ],
   };
 };
